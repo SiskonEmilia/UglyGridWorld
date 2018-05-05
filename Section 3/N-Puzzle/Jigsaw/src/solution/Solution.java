f21cd47de535b06c709614d1b2232f16a70a0108
SiskonEmilia <xiongyq7@mail2.sysu.edu.cn>
2018-05-05 09:31:22 +0800

    comments

--- Section 3/N-Puzzle/Jigsaw/src/solution/Solution.java
+++ Section 3/N-Puzzle/Jigsaw/src/solution/Solution.java
@@ -1,4 +1,4 @@
-// package solution;
+package solution;
 
 import java.util.LinkedList;
 import java.util.Queue;
--- Section 3/Sonar.sh
+++ Section 3/Sonar.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 cd ImageReader && sonar-runner
 cd ../MazeBug && sonar-runner
-cd ../N-Puzzle && sonar-runner
+cd ../N-Puzzle/Jigsaw/src/solution && sonar-runner
+cd ../../../../
